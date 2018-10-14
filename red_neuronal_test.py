8b429800c804cda78920351a56c0bffc6f942154
marisemeraro <mari.semeraro27@gmail.com>
2018-10-13 20:45:31 -0600

    Creacion modulo modelo

--- red_neuronal_test.py
+++ red_neuronal_test.py
@@ -4,10 +4,10 @@ def main():
     red_neuronal.leer_archivo()
     red_neuronal.normalizar()
     red_neuronal.crear_set_datos()
-    red_neuronal.separar_set_datos()
-    red_neuronal.crear_modelo()
-    red_neuronal.entrenar_modelo()
-    red_neuronal.probar_modelo()
+    #red_neuronal.separar_set_datos()
+    #red_neuronal.crear_modelo()
+    #red_neuronal.entrenar_modelo()
+    #red_neuronal.probar_modelo()
 
 if __name__ == "__main__":
     main()
