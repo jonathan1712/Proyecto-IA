d9769fe85273855ec3fa931128fe43f535476282
Tenorio <diegotenorio88@gmail.com>
2018-10-19 22:21:46 -0600

    modificacion 3 para merge

--- cross_validation_test_red_neuronal.py
+++
@@ -1,9 +0,0 @@
-from cross_validation import *
-
-
-def main():
-    cross_validation = Cross_Validation(1, 0.5, 1, 1)
-    cross_validation.cross_validation()
-
-if __name__ == "__main__":
-    main()
