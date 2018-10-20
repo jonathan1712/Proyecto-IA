4362fe26caee0ccd38765c33f0409b3003bd66a4
Tenorio <diegotenorio88@gmail.com>
2018-10-19 21:12:47 -0600

    Arreglo problema pull

--- cross_validation_test_red_neuronal.py
+++
@@ -1,9 +0,0 @@
-from cross_validation import *
-
-
-def main():
-    cross_validation = Cross_Validation(3, "red_neuronal", "prueba.csv")
-    cross_validation.cross_validation()
-
-if __name__ == "__main__":
-    main()
